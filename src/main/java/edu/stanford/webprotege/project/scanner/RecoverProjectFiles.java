1b0e070d2f84c74b46f28db5e6efc9fbc9cc706d
Matthew Horridge <matthew.horridge@stanford.edu>
2014-04-01 13:45:51 -0700

    Added checks for correct arguments

--- src/main/java/edu/stanford/webprotege/project/scanner/RecoverProjectFiles.java
+++ src/main/java/edu/stanford/webprotege/project/scanner/RecoverProjectFiles.java
@@ -2,6 +2,7 @@ package edu.stanford.webprotege.project.scanner;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 /**
  * @author Matthew Horridge, Stanford University, Bio-Medical Informatics Research Group, Date: 02/03/2014
@@ -10,15 +11,29 @@ public class RecoverProjectFiles {
 
 
     public static final String CHANGE_DATA_CHANGE_DATA_FILE_PATH = "/change-data/change-data.binary";
+    
+    private final PrintStream out;
 
     private File projectDirectory;
 
     public RecoverProjectFiles(File projectDirectory) {
         this.projectDirectory = projectDirectory;
+        out = System.out;
     }
 
     public static void main(String[] args) throws Exception {
+        if(args.length != 1) {
+            System.out.println("Expected one argument (project directory path)");
+            return;
+        }
         File file = new File(args[0]);
+        if(!file.exists()) {
+            System.out.printf("Directory does not exist (%s)", file);
+            return;
+        }
+        if(!file.isDirectory()) {
+            System.out.printf("Expected path to directory (%s)", file);
+        }
         RecoverProjectFiles recoverProjectFiles = new RecoverProjectFiles(file);
         recoverProjectFiles.recoverFiles();
     }
@@ -27,17 +42,17 @@ public class RecoverProjectFiles {
         try {
             attemptToReadDocument("Root ontology", getRootOntologyDocument(), new BinaryOWLOntologyDocumentConsumer());
         } catch (FileNotFoundException e) {
-            System.out.println("Root ontology document does not exist: " + getRootOntologyDocument());
+            out.printf("Root ontology document does not exist: %s", getRootOntologyDocument());
         }
         try {
             attemptToReadDocument("Notes ontology", getNotesOntologyDocument(), new BinaryOWLOntologyDocumentConsumer());
         } catch (FileNotFoundException e) {
-            System.out.println("Notes ontology document does not exist: " + getNotesOntologyDocument());
+            out.printf("Notes ontology document does not exist: %s", getNotesOntologyDocument());
         }
         try {
             attemptToReadDocument("Change log", getChangeLogDocument(), new BinaryOWLOntologyChangeLogConsumer());
         } catch (FileNotFoundException e) {
-            System.out.println("Change log does not exist: " + getChangeLogDocument());
+            out.printf("Change log does not exist: %s", getChangeLogDocument());
         }
     }
 
